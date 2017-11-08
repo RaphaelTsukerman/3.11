5fb2a1fba74c327c518c3cfd42f4d3eeeb836ec0
RaphaelTsukerman <raphael@tsukerman.org>
2017-11-08 14:41:38 +0200

    3.12 start engenering

--- 3.11/3.11.cpp
+++ 3.11/3.11.cpp
@@ -26,20 +26,7 @@ int main()
 		cout << "Second number: ";
 		int f2, sh2, p2;
 		cin >> f2 >> sh2 >> p2;
-		f = f1 + f2;
-		sh = sh1 + sh2;
-		p = p1 + p2;
-		if (p>=20)
-		{
-			sh++;
-			p -= 20;
-		}
-		if (sh>=12)
-		{
-			f++;
-			sh -= 12;
-		}
-		cout << f << ", " << sh << ", " << p << endl;
+		
 		break;
 	}
 	case '-':
@@ -47,25 +34,7 @@ int main()
 		cout << "Second number: ";
 		int f2, sh2, p2;
 		cin >> f2 >> sh2 >> p2;
-		if (f2>f1)
-		{
-			cout << "negative result"<<endl;
-			break;
-		}
-		if (p2>p1)
-		{
-			sh1--;
-			p1 += 20;
-		}
-		if (sh2>sh1)
-		{
-			f1--;
-			sh1 += 12;
-		}
-		f = f1 - f2;
-		sh = sh1 - sh2;
-		p = p1 - p2;
-		cout << f << ", " << sh << ", " << p << endl;
+		
 		break;
 	}
 	case '*':
@@ -73,20 +42,7 @@ int main()
 		cout << "Input factor: ";
 		int factor;
 		cin >> factor;
-		f = f1*factor;
-		sh = sh1*factor;
-		p = p1*factor;
-		while (p >= 20)
-		{
-			sh++;
-			p -= 20;
-		}
-		while (sh >= 12)
-		{
-			f++;
-			sh -= 12;
-		}
-		cout << f << ", " << sh << ", " << p << endl;
+		
 		break;
 	}
 	case '/':
@@ -94,18 +50,7 @@ int main()
 		cout << "Input divider: ";
 		int divider;
 		cin >> divider;
-		p = (p1 + sh1 * 20 + f1 * 12 * 20) / divider;
-		while (p>=20)
-		{
-			sh++;
-			p -= 20;
-		}
-		while (sh >= 12)
-		{
-			f++;
-			sh -= 12;
-		}
-		cout << f << ", " << sh << ", " << p << endl;
+		
 		break;
 	}
 
