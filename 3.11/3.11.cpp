8826f4e643a0f277d8b382da5feb62e54b194eb5
RaphaelTsukerman <raphael@tsukerman.org>
2017-11-09 17:05:52 +0200

    full ready app

--- 3.11/3.11.cpp
+++ 3.11/3.11.cpp
@@ -11,77 +11,145 @@ int main()
 	float result;
 	char reply;
 
-
-	cout << "First fraction: ";
-	cin >> a >> b;
-	if (b==0)
+	do
 	{
-		cout << "Division by zero! Wrong!!!" << endl;
-		system("pause");
-		exit(0);
-	}
-	cout << "Second fraction: ";
-	cin >> c >> d;
-	if (d == 0)
-	{
-		cout << "Division by zero! Wrong!!!" << endl;
-		system("pause");
-		exit(0);
-	}
-
-	cout << "Operation (+, -, *, /): ";
-	char answer;
-	cin >> answer;
-
-	switch (answer)
-	{
-	case '+':
-	{
-		res1 = (a*d) + (b*c);
-		res2 = (b*d);
-		while ((res1%2==0) && (res2%2==0))
+		cout << "First fraction: ";
+		cin >> a >> b;
+		if (b == 0)
 		{
-			res1 /= 2;
-			res2 /= 2;
+			cout << "Division by zero! Wrong!!!" << endl;
+			system("pause");
+			exit(0);
 		}
-		while ((res1 % 3 == 0) && (res2 % 3 == 0))
+		cout << "Second fraction: ";
+		cin >> c >> d;
+		if (d == 0)
 		{
-			res1 /= 3;
-			res2 /= 3;
+			cout << "Division by zero! Wrong!!!" << endl;
+			system("pause");
+			exit(0);
 		}
-		for (int i = 5; i < 1000; i++)
+
+		cout << "Operation (+, -, *, /): ";
+		char answer;
+		cin >> answer;
+
+		switch (answer)
 		{
-			while ((res1 % i == 0) && (res2 % i == 0))
+		case '+':
+		{
+			res1 = (a*d) + (b*c);
+			res2 = (b*d);
+			while ((res1 % 2 == 0) && (res2 % 2 == 0))
 			{
-				res1 /= i;
-				res2 /= i;
+				res1 /= 2;
+				res2 /= 2;
 			}
+			while ((res1 % 3 == 0) && (res2 % 3 == 0))
+			{
+				res1 /= 3;
+				res2 /= 3;
+			}
+			for (int i = 5; i < 1000; i++)
+			{
+				while ((res1 % i == 0) && (res2 % i == 0))
+				{
+					res1 /= i;
+					res2 /= i;
+				}
+			}
+			cout << "Result is " << res1 << "/" << res2 << endl;
+			break;
 		}
-		cout << "Result is " << res1 << "/" << res2 << endl;
-		break;
-	}
-	case '-':
-	{
-		cout << "Result is " << (a*d) - (b*c) << "/" << (b*d) << endl;
-		break;
-	}
-	case '*':
-	{
-		cout << "Result is " << (a*c) << "/" << (b*d) << endl;
-		break;
-	}
-	case '/':
-	{
-		cout << "Result is " << (a*d) << "/" << (b*c) << endl;
-		break;
-	}
+		case '-':
+		{
+			res1 = (a*d - b*c);
+			res2 = (b*d);
+			while ((res1 % 2 == 0) && (res2 % 2 == 0))
+			{
+				res1 /= 2;
+				res2 /= 2;
+			}
+			while ((res1 % 3 == 0) && (res2 % 3 == 0))
+			{
+				res1 /= 3;
+				res2 /= 3;
+			}
+			for (int i = 5; i < 1000; i++)
+			{
+				while ((res1 % i == 0) && (res2 % i == 0))
+				{
+					res1 /= i;
+					res2 /= i;
+				}
+			}
+			cout << "Result is " << res1 << "/" << res2 << endl;
+			break;
+		}
+		case '*':
+		{
+			res1 = (a*c);
+			res2 = (b*d);
+			while ((res1 % 2 == 0) && (res2 % 2 == 0))
+			{
+				res1 /= 2;
+				res2 /= 2;
+			}
+			while ((res1 % 3 == 0) && (res2 % 3 == 0))
+			{
+				res1 /= 3;
+				res2 /= 3;
+			}
+			for (int i = 5; i < 1000; i++)
+			{
+				while ((res1 % i == 0) && (res2 % i == 0))
+				{
+					res1 /= i;
+					res2 /= i;
+				}
+			}
 
-	default:
-		cout << "Wrong choise!" << endl;
-		break;
+			cout << "Result is " << res1 << "/" << res2 << endl;
+			break;
+		}
+		case '/':
+		{
+			res1 = (a*d);
+			res2 = (b*c);
+			while ((res1 % 2 == 0) && (res2 % 2 == 0))
+			{
+				res1 /= 2;
+				res2 /= 2;
+			}
+			while ((res1 % 3 == 0) && (res2 % 3 == 0))
+			{
+				res1 /= 3;
+				res2 /= 3;
+			}
+			for (int i = 5; i < 1000; i++)
+			{
+				while ((res1 % i == 0) && (res2 % i == 0))
+				{
+					res1 /= i;
+					res2 /= i;
+				}
+			}
+			cout << "Result is " << res1 << "/" << res2 << endl;
+			break;
+		}
+
+		default:
+			cout << "Wrong choise!" << endl;
+			break;
+		}
+		do
+		{
+			cout << "once more?(y/n)";
+			cin >> reply;
+		} while (reply != 'y' && reply != 'n');
 	}
-	cout << "once more?(y/n)";
-		cin >> reply;
+	while (reply == 'y'&&reply!='n');
+
 	system("pause");
     return 0;
 }
